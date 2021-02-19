ca1efd299ab64190dd689cf5ebf16d8ff7e27ca4
Johannes UNGER <pucgenie@gmail.com>
2021-02-19 13:36:22 +0100

    loadLibrary on Linux fixed
    Somehow on Windows it loads automatically but on Linux we need to manually call loadLibrary.

--- src/main/java/net/pucgenie/sysinfo/systemctl/status/SystemctlStatusApplication.java
+++ src/main/java/net/pucgenie/sysinfo/systemctl/status/SystemctlStatusApplication.java
@@ -1,13 +1,39 @@
 package net.pucgenie.sysinfo.systemctl.status;
 
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SystemctlStatusApplication {
+@lombok.extern.slf4j.Slf4j
+public class SystemctlStatusApplication
+implements org.springframework.boot.ApplicationRunner
+{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SystemctlStatusApplication.class, args);
+	}
+
+	@Override
+	public void run(org.springframework.boot.ApplicationArguments args) throws Exception {
+		//log.debug("java.library.path: {}", System.getProperty("java.library.path"));
+		boolean skipDefaultLib = args.getOptionValues("skipDefaultLib") != null;
+		List<String> argLoadLibrary = args.getOptionValues("loadLibrary");
+		if (argLoadLibrary != null) {
+			Runtime.getRuntime().loadLibrary(argLoadLibrary.get(0));
+			skipDefaultLib = true;
+		}
+		var argLoad = args.getOptionValues("load");
+		if (argLoad != null) {
+			Runtime.getRuntime().load(argLoad.get(0));
+			skipDefaultLib = true;
+		}
+		if (!skipDefaultLib) {
+			System.loadLibrary("unix-java");
+		} else {
+			log.info("skipped loading default library");
+		}
 	}
 
 }
